2126e1fc1783e958693676443943c5fdf932fd9f
sheepman105 <68858816+FuriousBomb@users.noreply.github.com>
2024-08-23 17:43:57 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,10 +9,12 @@ def welcome_page():
     return render_template("index.html")
 
 @app.route('/emotionDetector')
-def detect():
-    req = request.args.get("textToAnalyze")
-    resp = emotion_detector(req)
-    return ("For the given statement, the system response is \n", str(resp))
+def detect(): 
+    req = request.args.get("textToAnalyze") 
+    resp = emotion_detector(req) 
+    inp = resp 
+    del resp["dominant_emotion"] 
+    return (("For the given statement, the system response is {}.".format(resp), "The dominant emotion is {}".format(inp.get("dominant_emotion"))))
 
 if __name__ == "__main__":
     app.run()
