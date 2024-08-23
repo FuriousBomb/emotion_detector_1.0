1a5da507c446028669d0eb56c6f54b34b363c3d3
sheepman105 <68858816+FuriousBomb@users.noreply.github.com>
2024-08-23 19:08:02 +0530

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,12 +6,14 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = inp, headers=header)
     res = response.json()
-    for x in res.values():
-        for y in x[0].values():
-            maxim = max(y.values())
-            l = list(y.keys())
-            y["dominant_emotion"] = l[list(y.values()).index(maxim)]
-            return y
-    
-    
-            
+    empty = {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None, "dominant_emotion": None}
+    stat = response.status_code
+    if(stat == 400):
+        return empty
+    else:
+        for x in res.values():
+            for y in x[0].values():
+                maxim = max(y.values())
+                l = list(y.keys())
+                y["dominant_emotion"] = l[list(y.values()).index(maxim)]
+                return y
